97448915fac35bad052a61bed495ec6e3d963aff
mbhatia <mbhatia@vt.edu>
2019-08-09 16:24:31 -0400

    rararaara

---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import ProjectCard from '../components/ProjectCard'
+import './index.css'
+// import tw from "tailwindcss"
+
+
+export default () =>  (
+	<div>
+	<ProjectCard
+		img=""  
+		title="Cyber Africa"
+		description="this is my description"
+	/>
+	<ProjectCard
+		img=""  
+		title="NLP Africa"
+		description="this is my description"
+	/>
+	<ProjectCard
+		img=""  
+		title="Analytics Africa"
+		description="this is my description"
+	/>
+	<ProjectCard
+		img=""  
+		title="Cloud Africa"
+		description="this is my description"
+	/>
+	<ProjectCard
+		img=""  
+		title="Educate Africa"
+		description="this is my description"
+	/>
+	<ProjectCard
+		img=""  
+		title="Business Africa"
+		description="this is my description"
+	/>
+	</div>
+	
+
+)
