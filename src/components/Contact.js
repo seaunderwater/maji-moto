e073def73c7d64c3b3ee01b73df7e4d44db7a56e
seaunderwater <mbhatia@vt.edu>
2019-09-01 00:15:25 -0400

    added contact section and componentified section titles

---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import SectionTitle from './SectionTitle'
+export default () => (
+	<div class="my-10">
+	<SectionTitle title="Contact Us"></SectionTitle>
+	</div>
+	)
---
+++ src/components/SectionTitle.js
@@ -0,0 +1,5 @@
+import React from 'react' 
+
+export default props => (
+	<h1 class="text-6xl text-center">{props.title} </h1>
+)
